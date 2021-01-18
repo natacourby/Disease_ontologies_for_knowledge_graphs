e1ddfc4e0ad3fcea54431185f8d493347b50b91b
Natalja Kurbatova <natalja.kurbatova@gmail.com>
2021-01-18 12:28:44 +0000

    Update add_terms.py

--- scripts/add_terms.py
+++ scripts/add_terms.py
@@ -1,3 +1,19 @@
+# Software "Disease_ontologies_for_knowledge_graphs" - a knowledge base solution that uses Grakn core and disease ontologies cross-references to allow easy switch between ontology hierarchies for data integration purpose.
+# Copyright (C) 2021  Natalja Kurbatova
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 #!/usr/bin/env python3
 from grakn.client import GraknClient
 import csv
